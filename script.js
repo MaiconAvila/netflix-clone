edc4642d31e4a45956ed439f23e7ea405ba07f0a
vai na web <vainawebcpx@gmail.com>
2019-11-22 12:37:25 -0300

    Etilização da lupa

--- script.js
+++ script.js
@@ -3,10 +3,29 @@ videoPrincipal.addEventListener("mouseenter", iniciarVideo);
 videoPrincipal.addEventListener("mouseout", terminarVideo);
 
 function iniciarVideo() {
-  videoPrincipal.src = "assets/twd.gif";
+  videoPrincipal.src = "assets/imagem-inicial.webp";
 }
 
 function terminarVideo() {
-  videoPrincipal.src = "assets/images (1).jfif";
+  videoPrincipal.src = "assets/imagem-inicial.webp";
 }
 /*===============*/
+let barraPesquisa = document.querySelector('.barraPesquisa')
+
+function abrirBarra() {
+  if("onclick") {
+    barraPesquisa.style.display = "flex";
+
+  }
+}
+
+let borderPesquisa = document.querySelector('.borderPesquisa')
+
+function borderBarra() {
+  if(barraPesquisa.style.display === "flex") {
+    borderPesquisa.style.border = "1 white"
+    borderPesquisa.style.padding = "5"
+  } else if(barraPesquisa.style.display === "none") {
+    borderPesquisa.style.border = "none"
+  }
+}
